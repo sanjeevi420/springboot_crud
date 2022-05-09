e73749c0ec499d840f307681568a6a0dd88f78ff
sanjeevi420 <74852597+sanjeevi420@users.noreply.github.com>
2022-05-09 11:52:08 +0530

    Update empserviceimp.java
    throws exception added

--- crudoperation/src/main/java/com/example/service/empserviceimp.java
+++ crudoperation/src/main/java/com/example/service/empserviceimp.java
@@ -5,14 +5,14 @@ import java.util.List;
 import com.example.model.employee;
 
 public interface empserviceimp {
-	List<employee> getallemp();
+	List<employee> getallemp() throws Exception;
 	
-	employee getEmployeeById(int id);
+	employee getEmployeeById(int id) throws Exception;
 	
-	void saveOrUpdate(employee emp);
+	void saveOrUpdate(employee emp) throws Exception;
 	
-	void delete(int id);
+	void delete(int id) throws Exception;
 	
-	void update(employee emp, int empid);
+	void update(employee emp, int empid) throws Exception;
 	
 }
