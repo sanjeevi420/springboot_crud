e45b0e4e49dbd291f233d115c3ed4987e5e9c1d8
sanjeevi420 <74852597+sanjeevi420@users.noreply.github.com>
2022-05-19 16:13:48 +0530

    updated repo test
    Added more methods

--- crudoperation/src/test/java/com/example/controller/RepoTest.java
+++ crudoperation/src/test/java/com/example/controller/RepoTest.java
@@ -33,7 +33,35 @@ public class RepoTest {
 		 List<employee> res = empctrl.getAllemployees();
 		 assertNotNull(res);
 	 }
-	 
+	
+	 @Test
+	 void noEmpInDb() {
+		 List<employee> res = empctrl.getAllemployees();
+		 List<employee> def = new ArrayList<employee>();
+		 assertEquals(res.size(), def.size());
+	 }
+	
+	 @Test
+	 void noEmpById() {
+		 employee Employee = new employee(3333,"Sam");
+		 empctrl.saveEmployee(Employee);
+		 employee actualResult = empctrl.getemployee(2233);
+		 assertNull(actualResult);
+		 empctrl.deleteEmployee(3333);
+	 }
+	
+	@Test
+	 void updateEmp() {
+		 employee Employee = new employee(1234,"Sam");
+		 int eid = empctrl.saveEmployee(Employee);
+		 employee Employee1 = new employee(1234,"Tam");
+		 empctrl.update(Employee1);
+		 String name=empctrl.getemployee(eid).getEmpname();
+		 assertEquals(name, "Tam");
+		 empctrl.deleteEmployee(1234);
+		 
+	 }
+	
 	 @Test
 	 void saveEmp() {
 		 employee Employee = new employee(1234);
